f7ce6f8abae1ee778097c641d29b0bd58166c94f
Rowalewa <lesliewanjala06@gmail.com>
2024-09-28 08:08:38 +0300

    caloriesCounter

--- script.js
+++ script.js
@@ -6,15 +6,45 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
+// you need to use the backslash \ character to escape the + symbol because it has a special meaning in regular expressions.
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
+// /\s/ - checks for white space
+/* This pattern won't work just yet. /\+-\s/ looks for +, -, and a space in order. This would match +- hello but would not match +hello.
+To tell the pattern to match each of these characters individually, you need to turn them into a character class. This is done by wrapping the characters you want to match in brackets. [+-\s]
+Example Code
+const regex = /[helo]/;
+Turn your +-\s pattern into a character class. 
+No longer need to escape the + character, because you are using a character class.
+
+Regex can also take specific flags to alter the pattern matching behavior. Flags are added after the closing /. The g flag, which stands for "global", will tell the pattern to continue looking after it has found a match. Here is an example:
+Example Code
+const helloRegex = /hello/g;
+*/
+
 
 function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
+
+// + after the character d is for other occurences after it eg 10, 0 after 1.
+/* Strings have a .match() method, which takes a regex argument. .match() will return an array of match results – containing either the first match, or all matches if the global flag is used.
+Example Code
+const str = 'example string';
+const regex = /example/;
+const result = str.match(regex); // Returns ['example']
+Return the result of calling the .match() method on str and passing your regex variable as the argument. You'll use this match result later on.
+ 
+The match method returns an array with any matches found in the string.
+Here is a complete breakdown of that information:
+* "1e3" is the matched value against the /\d+e\d+/i regex.
+* index: 0 is the index of the matched value in the string.
+* input: '1e3' is the original string that was matched.
+* groups: undefined are the matched groups, which are not used in this case.
+*/
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
@@ -31,6 +61,12 @@ function addEntry() {
   />`;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+/* The insertAdjacentHtml method takes two arguments. The first argument is a string that specifies the position of the inserted element. The second argument is a string containing the HTML to be inserted.The insertAdjacentHtml method takes two arguments. The first argument is a string that specifies the position of the inserted element. The second argument is a string containing the HTML to be inserted.
+
+ You will want to number the entries a user adds. To get all of the number inputs, you can use the querySelectorAll() method.
+The querySelectorAll() method returns a NodeList of all the elements that match the selector. A NodeList is an array-like object, so you can access the elements using bracket notation.
+ */
 
 function calculateCalories(e) {
   e.preventDefault();
@@ -79,6 +115,8 @@ function getCaloriesFromInputs(list) {
       isError = true;
       return null;
     }
+    /* Values from an HTML input field are received as strings in JavaScript. 
+    You'll need to convert these strings into numbers before performing any calculations.*/
     calories += Number(currVal);
   }
   return calories;
